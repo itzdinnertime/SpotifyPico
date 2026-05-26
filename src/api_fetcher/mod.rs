df0bab3cc29f6a1edc6726602fc6015de122878e
Jason <chenjason214@gmail.com>
2026-05-26 03:17:08 -0400

    Added the fetch function to get the currently playing endpoint on spotify

---
+++ src/api_fetcher/mod.rs
@@ -0,0 +1,19 @@
+use crate::NowPlaying;
+
+async fn get_current_playing(
+    client: &reqwest::Client,
+    token: &str,
+) -> Result<Option<NowPlaying>, reqwest::Error> {
+    let url = "https://api.spotify.com/v1/me/player/currently-playing";
+    let response = client.get(url).bearer_auth(token).send().await?;
+
+    if response.status().as_u16() == 204 {
+        Ok(None)
+    } else if response.status().as_u16() == 200 {
+        let track = response.json::<NowPlaying>().await?;
+        return Ok(Some(track));
+    } else {
+        eprintln!("Request failed: {}", response.status());
+        Ok(None)
+    }
+}
