7b5d5c89e6dbf40e0c5cd237cecddd592e1e7b3c
Jason <chenjason214@gmail.com>
2026-05-29 03:17:41 -0400

    Added build_auth_url function

--- src/api_fetcher/spotify_auth.rs
+++ src/api_fetcher/spotify_auth.rs
@@ -15,3 +15,12 @@ pub fn generate_code_challenge(verifier: &str) -> String {
     let encoded = URL_SAFE_NO_PAD.encode(&hashed);
     return encoded;
 }
+
+/// Builds the Spotify authorization URL.
+pub fn build_auth_url(client_id: &str, code_challenge: &str) -> String {
+    let url = format!(
+        "https://accounts.spotify.com/authorize?client_id={}&code_challenge={}&response_type=code&redirect_uri={}&code_challenge_method=S256&scope=user-read-currently-playing",
+        client_id, code_challenge, "http://localhost:8888/callback"
+    );
+    return url;
+}
