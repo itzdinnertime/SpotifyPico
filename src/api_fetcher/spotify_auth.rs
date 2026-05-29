833cb3b5a9446b88e6167092e3c2aa4dac2bab39
Jason <chenjason214@gmail.com>
2026-05-28 22:00:05 -0400

    added generate_code_verifier function

--- src/api_fetcher/spotify_auth.rs
+++ src/api_fetcher/spotify_auth.rs
@@ -1,1 +1,8 @@
+use base64::{Engine, engine::general_purpose::URL_SAFE_NO_PAD};
+use rand::random;
 
+fn generate_code_verifier() -> String {
+    let gen_code: [u8; 64] = random();
+    let encoded = URL_SAFE_NO_PAD.encode(&gen_code);
+    return encoded;
+}
