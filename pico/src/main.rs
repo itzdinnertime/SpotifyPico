3cfb1da9ad77e4ace74826dac161fcaff324ed2a
Jason <chenjason214@gmail.com>
2026-06-17 22:35:42 -0400

    initializing the CYW43 driver

--- pico/src/main.rs
+++ pico/src/main.rs
@@ -1,10 +1,34 @@
 #![no_std]
 #![no_main]
 
+use cyw43_pio::PioSpi;
 use defmt_rtt as _;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::pio::Pio;
 use panic_probe as _;
+
+embassy_rp::bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<embassy_rp::peripherals::PIO0>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: embassy_executor::Spawner) {
     let p = embassy_rp::init(Default::default());
+    let fw = include_bytes!("../firmware/43439A0.bin");
+    let clm = include_bytes!("../firmware/43439A0_clm.bin");
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
 }
