e520f4614fff1953908da6d9b0f9672794832b5d
Jason <chenjason214@gmail.com>
2026-06-18 18:53:42 -0400

    did too much stuff in this commit

--- pico/src/main.rs
+++ pico/src/main.rs
@@ -6,13 +6,30 @@ use defmt_rtt as _;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::pio::Pio;
 use panic_probe as _;
+use static_cell::StaticCell;
 
 embassy_rp::bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<embassy_rp::peripherals::PIO0>;
 });
 
+#[embassy_executor::task]
+async fn wifi_task(
+    runner: cyw43::Runner<
+        'static,
+        Output<'static>,
+        PioSpi<'static, embassy_rp::peripherals::PIO0, 0, embassy_rp::peripherals::DMA_CH0>,
+    >,
+) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static embassy_net::Stack<cyw43::NetDriver<'static>>) -> ! {
+    stack.run().await
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: embassy_executor::Spawner) {
+async fn main(spawner: embassy_executor::Spawner) {
     let p = embassy_rp::init(Default::default());
     let fw = include_bytes!("../firmware/43439A0.bin");
     let clm = include_bytes!("../firmware/43439A0_clm.bin");
@@ -30,5 +47,29 @@ async fn main(_spawner: embassy_executor::Spawner) {
     );
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
-    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    let (net_device, control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    spawner.spawn(wifi_task(runner)).unwrap();
+    let mut control = control;
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    let config = embassy_net::Config::dhcpv4(Default::default());
+
+    static RESOURCES: StaticCell<embassy_net::StackResources<2>> = StaticCell::new();
+    static STACK: StaticCell<embassy_net::Stack<cyw43::NetDriver<'static>>> = StaticCell::new();
+
+    let stack = &*STACK.init(embassy_net::Stack::new(
+        net_device,
+        config,
+        RESOURCES.init(embassy_net::StackResources::new()),
+        1234,
+    ));
+    control
+        .join_wpa2("YOUR_WIFI_SSID", "YOUR_WIFI_PASSWORD")
+        .await
+        .unwrap();
+
+    spawner.spawn(net_task(stack)).unwrap();
 }
