c1d30e9b109a8b3f301436b93ece3793b9b85f68
jason chen <chenjason214@gmail.com>
2026-06-30 22:01:56 -0400

    Added base main.rs code and cargo.toml features and dependecies

---
+++ pico/src/main.rs
@@ -0,0 +1,11 @@
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _;
+use panic_probe as _;
+
+#[embassy_executor::main]
+async fn main(_spawner: embassy_executor::Spawner) {
+    let _p = embassy_rp::init(Default::default());
+    defmt::info!("Hello from Pico!");
+}
